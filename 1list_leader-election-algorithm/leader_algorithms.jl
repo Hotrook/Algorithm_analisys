7f3a2f86cc09033dcc9593e7fa0e0c3fc2d44bcd
Hotrook <mrozsebastian@wp.pl>
2018-02-21 22:20:10 +0100

    add algorithm for second scenario, when n is not known

--- 1list_leader-election-algorithm/leader_algorithms.jl
+++ 1list_leader-election-algorithm/leader_algorithms.jl
@@ -16,3 +16,25 @@ function leader_firstscenario(n::Integer)
 
     return steps
 end
+
+function leader_secondscenario(u::Integer, n::Integer)
+    d = ceil(Int64, log( u ))
+    probabilities = Array{Float64, 1}( d )
+    probabilities[ 1 ] = 1/2
+    for i = 2 : d
+        probabilities[ i ] = probabilities[ i - 1 ] * 0.5
+    end
+
+    steps = 0
+    transmitted = 0
+    while transmitted != 1
+        transmitted = 0
+        for i = 1:n
+            if rand() < probabilities[ ( (steps) % d ) + 1 ]
+                transmitted += 1
+            end
+        end
+        steps += 1
+    end
+    return steps
+end
--- 1list_leader-election-algorithm/z1.jl
+++ 1list_leader-election-algorithm/z1.jl
@@ -1,12 +1,20 @@
 using Gadfly
 include( "leader_algorithms.jl")
 
+u = 1000
 n = 1000
-probes = 10000
+probes = 1000
 results = Array{Int64}(probes)
 
+# for i = 1:probes
+#     results[ i ] = leader_firstscenario( n )
+# end
+#
+# Gadfly.plot( x = results, Geom.histogram )
+
 for i = 1:probes
-    results[ i ] = leader_firstscenario( n )
+    results[ i ] = leader_secondscenario( u, n )
+    println( i )
 end
 
 Gadfly.plot( x = results, Geom.histogram )
