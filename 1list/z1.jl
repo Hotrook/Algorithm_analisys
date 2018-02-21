998f89db9f1660f2b407f656b69aac2c91177f2a
Hotrook <mrozsebastian@wp.pl>
2018-02-21 01:50:35 +0100

    check

--- 1list/z1.jl
+++ 1list/z1.jl
@@ -1,35 +1,12 @@
 using Gadfly
-n = 100000
-probes = 1000
+include( "leader_algorithms.jl")
 
-function leader_firstscenario(n::Integer)
-    probability = 1 / n
-    leader_found = false
-    steps = 0
-    transmitted = 0
-
-    while !leader_found
-        transmitted = 0
-        for i = 1:n
-            r = rand()
-            if r < probability
-                transmitted += 1
-            end
-        end
-        steps += 1
-        if transmitted == 1
-            leader_found = true
-        end
-    end
-
-    return steps
-end
-
+n = 1000
+probes = 10000
 results = Array{Int64}(probes)
 
 for i = 1:probes
     results[ i ] = leader_firstscenario( n )
 end
-
 
 Gadfly.plot( x = results, Geom.histogram )
